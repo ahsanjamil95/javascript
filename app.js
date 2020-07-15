04a902ac3e06fb0bf45e4bb73583c95ab1f0b959
Ahsan Jamil <ahsanjamil@live.com>
2020-07-15 20:51:27 +0500

    js

---
+++ app.js
@@ -0,0 +1,15 @@
+//function sum(a,b){
+ //   alert(a+b);
+//}
+
+//sum(3,3);
+
+function areaCal(a,b,c){
+      var s = (a+b+c)/2;
+      var area =Math.sqrt( s*((s-a)*(s-b)*(s-c)));
+      alert(area);
+ 
+
+}
+
+areaCal(56,54,76);
